12ab7c2f07bbb837187530468650229fd63f9743
Liang Zhenkai <liangzk@comp.nus.edu.sg>
2013-12-10 17:59:49 +0800

    add gcc-ndk.sh

---
+++ gcc-ndk.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.6
+git clone https://android.googlesource.com/platform/prebuilts/ndk
