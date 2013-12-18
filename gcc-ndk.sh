3cb2d851379a23e8c0b01e1777cdff46acd8851c
Liang Zhenkai <liangzk@comp.nus.edu.sg>
2013-12-18 21:33:40 +0800

    gcc-ndk

--- gcc-ndk.sh
+++ gcc-ndk.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-if [ $? -eq 0 ]
+if [ $# -eq 0 ]
 then
     git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.6
     git clone https://android.googlesource.com/platform/prebuilts/ndk
 elif [ "$1" == "http" ]
 then
-elif [ "$1" == "http" ]
+    echo "Downloanding over HTTP ..."
     git clone http://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.6
     git clone http://android.googlesource.com/platform/prebuilts/ndk
 elif [ "$1" == "cache" ]
 then
+    echo "Downloading from cache ..."
     wget http://www.comp.nus.edu.sg/~liangzk/downloads/gcc-ndk.tar.gz
-    tar zxvf gcc-ndk.tar.gz
+    tar zxvf gcc-ndk.tar.gz > /dev/null
 fi
