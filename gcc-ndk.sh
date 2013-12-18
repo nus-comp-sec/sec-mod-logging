2c48c9388e633907127ec18e58be2b92c4f01e36
Liang Zhenkai <liangzk@comp.nus.edu.sg>
2013-12-18 21:28:28 +0800

    cached ndk gcc

--- gcc-ndk.sh
+++ gcc-ndk.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.6
-git clone https://android.googlesource.com/platform/prebuilts/ndk
+if [ $? -eq 0 ]
+then
+    git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.6
+    git clone https://android.googlesource.com/platform/prebuilts/ndk
+elif [ "$1" == "http" ]
+then
+elif [ "$1" == "http" ]
+    git clone http://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.6
+    git clone http://android.googlesource.com/platform/prebuilts/ndk
+elif [ "$1" == "cache" ]
+then
+    wget http://www.comp.nus.edu.sg/~liangzk/downloads/gcc-ndk.tar.gz
+    tar zxvf gcc-ndk.tar.gz
+fi
